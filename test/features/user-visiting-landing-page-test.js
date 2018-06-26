80b665839276a2454e99b47b911cdbd493aa930c
Jacob Honore <jhonore@deloitte.dk>
2018-06-26 19:41:30 +0200

    Task 6: Test that users can navigate to create-page and write minimum code to pass

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -10,4 +10,16 @@ describe('User visits landing page', () => {
       assert.equal(browser.getText('#videos-container'), '');
     });
   });
+  describe('navigates to videos/create.html page', () => {
+    it('contains the text Save a video', () => {
+      // Setup
+      browser.url('/');
+
+      // Exercise
+      browser.click('a[href="/videos/create.html"]');
+
+      // Verification
+      assert.include(browser.getText('body'), 'Save a video');
+    });
+  });
 });
