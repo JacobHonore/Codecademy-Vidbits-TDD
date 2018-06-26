86d68f3ced82f281b3ee93bf76359c7d3bd3b7ce
Jacob Honore <jhonore@deloitte.dk>
2018-06-26 19:26:04 +0200

    Add task 3 - test for checking if videos container is empty

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,13 @@
 const {assert} = require('chai');
+
+describe('User visits landing page', () => {
+  describe('without existing videos', () => {
+    it('videos container is empty', () => {
+      // Setup
+      browser.url('/');
+
+      // Exercise & Verification
+      assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+});
