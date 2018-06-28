11a4c66dbbee2346fa34dc14e7c019707586d0f7
Jacob Honore <jhonore@deloitte.dk>
2018-06-28 19:02:53 +0200

    Task 10: Rename to video-test and create failing test that creates object

---
+++ test/routes/video-test.js
@@ -0,0 +1,45 @@
+const {assert} = require('chai');
+const request = require('supertest');
+const app = require('../../app');
+const {buildItemObject} = require('../test-utils');
+const {connectDatabase, disconnectDatabase} = require('../database-utilities');
+
+describe('Server path: /videos', () => {
+  beforeEach(async () => {
+    await mongoose.connect(databaseUrl, options);
+    await mongoose.connection.db.dropDatabase();
+  });
+
+  afterEach(async () => {
+     await mongoose.disconnect();
+  });
+  describe('POST', () => {
+    it('responds with created status', async () => {
+      // Setup
+      const itemToCreate = buildItemObject();
+
+      // Exercise
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(itemToCreate);
+
+      // Verification
+      assert.equal(response.status, 201);
+    });
+    it('creates object in database after posting', async() => {
+      // Setup
+      const itemToCreate = buildItemObject();
+
+      // Exercise
+      const response = await request(app)
+       .post('/videos')
+       .type('form')
+       .send(itemToCreate);
+
+     // Verification
+     const createdItem = await Video.findOne(itemToCreate);
+     assert.isOk(createdItem, 'Item was not created successfully in the database');
+    });
+  });
+});
