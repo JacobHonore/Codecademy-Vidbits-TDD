ee88c3e51d1d22ff189a3e06263c8c155119424b
Jacob Honore <jhonore@deloitte.dk>
2018-06-28 19:11:41 +0200

    Task 11: Make test for Video title is a string and make it green

--- test/models/video-test.js
+++ test/models/video-test.js
@@ -1,5 +1,6 @@
 const {assert} = require('chai');
 const {mongoose, databaseUrl, options} = require('../../database');
+const Video = require('../../models/video')
 
 async function connectDatabase() {
   await mongoose.connect(databaseUrl, options);
@@ -14,3 +15,19 @@ module.exports = {
   connectDatabase,
   disconnectDatabase,
 }
+
+describe('Model: Video', () => {
+  beforeEach(connectDatabase);
+  afterEach(disconnectDatabase);
+
+  describe('#title', () => {
+    it('should be a string', () => {
+      // Setup
+      const notString = 1;
+      const item = new Video({title: notString});
+
+      // Verification
+      assert.strictEqual(item.title, notString.toString());
+    });
+  });
+});
